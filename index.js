a047c70270d7143afee7230278a552af2b0abcaa
mbdullah <mdabdullah162005@gmail.com>
2025-05-20 22:33:50 +0600

    includes a groupCollection and create a post api

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const groupCollection = client.db("groupDB").collection("groups");
+
+    app.post("/groups", async(req, res) => {
+      const group = req.body;
+      const result = await groupCollection.insertOne(group);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
